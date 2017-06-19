c757f9ae2d9dedc91dec36a8b8b3ee6f24c1d547
miyuhann <tennkunn96@gmail.com>
2017-06-19 17:27:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,17 @@
 
-var result = window.confirm('準備OK?');
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください!'));
 
-if(result){
-document.getElementById('choice').textContent = 'OKが押されました。';
+var message;
+if(answer === number) {
+  message = 'あたり！';
+}else if (answer < number){
+  message = '残念！もっと大きいです。';
+}else if (number < answer){
+  message = '残念！もっと小さいです。';
 }else{
-  document.getElementById('choice').textContent = 'キャンセルが押されました。';
+  message = '0~3の数字を入力してください。';
 }
+
+
+docment.getElementById('choice').textContent = message;
